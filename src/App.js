4512d04542e872229ea2a6eaf53f2cfc9bafa7f9
Manoranjan710 <manoranjan710@gmail.com>
2023-06-13 16:14:02 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, {useState} from 'react';
+// import Confetti from 'react-confetti'
 
 function App() {
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [bmi, setBmi] = useState("");
+  const [message, setMessage] = useState("");
+
+  let calcBmi = (e) => {
+    
+    e.preventDefault();
+
+    if (weight===0 || height===0){
+      alert("Please enter a valid height and weight")
+    }
+    else{
+      let bmi = (weight/(height*height))
+      setBmi(bmi.toFixed(1))
+
+      if (bmi<18.5){
+        setMessage("You are under-weight")
+      }
+      else if( bmi>=18.5 && bmi<25 ){
+        setMessage("You are healthy")
+      }
+      else{
+        setMessage("You are over-weight")
+      }
+    }
+  }
+
+  let imgSrc;
+
+  if (bmi<1){
+    imgSrc = null
+  } else{
+    if(bmi<18.5){
+        imgSrc = require("../src/assets/underweight.png")
+    }
+    else if(bmi>=18.5 && bmi<25 ){
+      imgSrc = require("../src/assets/healthy.png")
+    }
+    else{
+      imgSrc = require("../src/assets/overweight.png")
+    }
+  }
+
+  let reload = () =>{
+    window.location.reload()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    {/* { setMessage("You are healthy") && <Confetti />} */}
+
+      <div className="container">
+        <h2>BMI Calculator</h2>
+        <form onSubmit={calcBmi}>
+          <div>
+            <label> Weight (kgs) </label>
+            <input
+              type="text"
+              placeholder="Enter weight value"
+              value={weight}
+              onChange={(e)=> setWeight(e.target.value)}
+            />
+          </div>
+          <div>
+            <label> Height (mts) </label>
+            <input
+              type="text"
+              placeholder="Enter height value"
+              value={height}
+              onChange={(e)=> setHeight(e.target.value)}
+            />
+          </div>
+
+          <div>
+            <button className="btn" type="submit">
+              Submit
+            </button>
+            <button className="btn btn-outline" type="submit" onClick={reload}>
+              Reload
+            </button>
+          </div>
+
+          <div className="center">
+            <h3> Your BMI is: {bmi}</h3>
+            <p> {message} </p>
+          </div>
+
+          <div className="image-conatiner center">
+            <img src={imgSrc} alt="" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
